4e6ffa7db787d8fc74e208a10ad9a2ed8a47b940
vsbakula <goldenrhesus2022@gmail.com>
2019-04-05 17:54:25 -0700

    first coding html/js

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,4 +1,25 @@
-// ToDo make keys 1st crystal, 2nd Crystal, 3 Crystal, 4th Crysttal
+// ToDo  display losses
+var losses = 0;
+// Todo display show wins
+var wins = 0;
+console.log()
+// ToDo  display count 
+// ToDo  display randon computer number to match
+var ruby;
+var emerald;
+var orange sapphire
+var purple sapphire;
+// ToDo  back ground photo
+// ToDo boxes for each score/instruction/h1/ wins-losses and jewels
+// 
 
-// Todo 
+
+// score to match (random number 19-120)
+//  function to click crystal to add score.
+// make keys click on events for 1st crystal, 2nd Crystal, 3 Crystal, 4th Crysttal
+for (var i =0; i >4: i++)
+// function to make random crystal numbers 1-12, each time resets a new randome number selected!!!!
+// function that if score equal the computer random # wins (score goes up)
+// function if the score does not equal the computer randon # Loss (score goes up)
+// restart game when win or loss happens...
 // 
