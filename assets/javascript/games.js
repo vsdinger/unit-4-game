b20840736ef714f6fc5acca24a781ade8a448267
vsbakula <goldenrhesus2022@gmail.com>
2019-04-06 23:04:25 -0700

    end of times

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,29 +1,69 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    // ToDo  display losses
+    // Todo display show wins
+    // console.log()
+    // ToDo  display count 
+    // ToDo  display randon computer number to match
+    var numbertoguess = [];
+    // score to match (random number 19-120)
+    for (var n = 19; n < 121; n++) {
+        numbertoguess.push(n);
+    }
+    // console.log ()
+    //  function to click crystal to add score.
+    var crystals = [];
 
+    for (var c = 1; c < 13; c++) {
+        crystals.push(c);
+    }
 
-// ToDo  display losses
-var losses = 0;
-// Todo display show wins
+// random var selected by computer crystals:
+  var yourCount;  //number to match
+
+var crystalNumber = [];
+
+var c1;
+var c2;
+var c3;
+var c4;
+
+var totalScore = 0;  //user score
 var wins = 0;
-console.log()
-// ToDo  display count 
-// ToDo  display randon computer number to match
-var ruby;
-var emerald;
-var orange sapphire
-var purple sapphire;
-// ToDo  back ground photo
-// ToDo boxes for each score/instruction/h1/ wins-losses and jewels
-// 
+var losses = 0;
 
+//function
+function pickYourCount(arr) {
+    var x = arr[Math.floor(Math.random() * arr.length)];
+    yourCount = x;
+    $("#yourCount").html(yourCount);
 
-// score to match (random number 19-120)
-//  function to click crystal to add score.
-// make keys click on events for 1st crystal, 2nd Crystal, 3 Crystal, 4th Crysttal
-for (var i =0; i >4: i++)
-// function to make random crystal numbers 1-12, each time resets a new randome number selected!!!!
-// function that if score equal the computer random # wins (score goes up)
-// function if the score does not equal the computer randon # Loss (score goes up)
-// restart game when win or loss happens...
+    console.log("yourCount: " + yourCount);
+
 }
+
+//random num for crystals
+
+Function pickRandomCrystals(arr) {
+    for (var i = 0; i < 4: i++){
+        var a = arr[Math.floor(Math.random() * arr.length)];
+        crystalNumber.push(a);
+    }
+//check
+console.log("crystal numbers: " + crystalNumber);
+}
+
+function crystalValues(arr) {
+    for (i = 0; i < arr.length; i++) {
+
+        $("#button-" + (i+1)).attr("value", arr[i]);
+        console.log(this);
+    }
+    c1 = arr[0];
+    c2 = arr[1];
+    c3 = arr[2];
+    c4 = arr[3];
+}
+
+
+});
